3bde63104570228cdcd69000a2a6645ffc24a066
EagleEye-0101 <kaliking2k20@gmail.com>
2025-12-05 11:40:40 +0530

    Initial Commit

---
+++ Loginn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Loginn
+//
+//  Created by students on 14/9/47.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
